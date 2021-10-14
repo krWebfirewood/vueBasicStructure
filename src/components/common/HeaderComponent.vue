5b7223bf952d9d5819fc8afd5f89946d3a27d384
jaehong <webfirewood@blackholic.com>
2021-10-14 11:14:29 +0900

    eslint test

--- src/components/common/HeaderComponent.vue
+++ src/components/common/HeaderComponent.vue
@@ -1,23 +1,17 @@
 <template>
   <div>
     <ul>
-      <li>
-        1
-      </li>
-      <li>
-        2
-      </li>
-      <li>
-        3
-      </li>
-      <li>
-        4
-      </li>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
     </ul>
   </div>
 </template>
 
 <script>
+const abc = 0;
+
 export default {
   name: 'HeaderComponent',
 };
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,1 @@
-module.exports = {
-	devServer: {
-		overlay: false,
-	},
-};
+module.exports = {};
