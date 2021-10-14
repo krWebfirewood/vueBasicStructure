ce57fc6861848568ebdc509413165e3ec7da81e5
jaehong <webfirewood@blackholic.com>
2021-10-14 13:47:15 +0900

    test2

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,16 +9,9 @@ module.exports = {
   parserOptions: {
     parser: 'babel-eslint',
   },
+  ignorePatterns: ['node_modules/'],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'prettier/prettier': 'error',
-    semi: 0,
-    'import/no-unresolved': 'off', //require.resolve동작에 정의 된대로 로컬 파일 시스템의 모듈로 해석 할 수 있도록 한다
-    'comma-dangle': 'off', //맨 뒤에 붙은 쉼표를 허용을 하지 않는다
-    'no-new': 0, //new 키워드를 이용해 생성자를 호출했다면 변수에 담아야 한다
-    indent: ['error', 2], //indent를 2로 설정한다는 것으며, 2가 아닌 경우 error를 발생시키라는 의미
-    'import/extensions': 'off', //확장자 명을 명시할 것인지에 대한 설정
   },
   overrides: [
     {
