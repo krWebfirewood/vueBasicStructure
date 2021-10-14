503ce8b3f0c323e5daa7abfb2e58b86a0253924f
jaehong <webfirewood@blackholic.com>
2021-10-14 11:46:04 +0900

    test

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,10 @@
-import axios from "axios";
+import axios from 'axios';
 
 export function createInstance() {
-    return axios.create({
-        baseURL: process.env.VUE_APP_API_PRODUCT_URL,
-    });
+  return axios.create({
+    baseURL: process.env.VUE_APP_API_PRODUCT_URL,
+  });
 }
 
+const abc = 0;
 export const instance = createInstance();
